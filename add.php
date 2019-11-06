4146dec0e48f510518452923d113d9b036dcce2f
mc-santiago <mari.c.santiago@europe.com>
2019-11-06 19:01:22 +0100

    display error messages on the form

--- add.php
+++ add.php
@@ -1,55 +1,59 @@
 <?php
 // POST METHOD.... 
-// htmlspecialchars is a security measure when not validating... example: echo htmlspecialchars($_POST['email']);
-if ( isset($_POST['submit']) ) {
+// htmlspecialchars is a security measure... example: echo htmlspecialchars($_POST['email']);
+
+$email = $title = $image = $description = $languages = '';
+$errors = array('email'=>'', 'title'=>'', 'image'=>'', 'description'=>'', 'languages'=>'');
+
+if(isset($_POST['submit'])) {
 
     // Check email
     if(empty($_POST['email'])) {
-        echo "An email is required <br />";
+        $errors['email'] = "An email is required <br />";
     } else {
         $email = $_POST['email'];
         if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            echo 'Must enter a valid email address.';
+            $errors['email'] = 'Must enter a valid email address.';
         }
     }
 
     // Check title
     if(empty($_POST['title'])) {
-        echo "A title is required <br />";
+        $errors['title'] = "A title is required <br />";
     } else {
         $title = $_POST['title'];
 		if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
-			echo 'Title must be letters and spaces only.';
+			$errors['title'] = 'Title must be letters and spaces only.';
 		}
     }
 
     // Check image url
     if(empty($_POST['image'])) {
-        echo "An image url is required <br />";
+        $errors['image'] = "An image url is required <br />";
     } else {
         $image = $_POST['image'];
         if(!filter_var($image, FILTER_VALIDATE_URL)){
-            echo 'Must enter a valid URL.';
+            $errors['image'] = 'Must enter a valid URL.';
         }
     }
 
     // Check description
     if(empty($_POST['description'])) {
-        echo "A description is required <br />";
+        $errors['description'] = "A description is required <br />";
     } else {
         $description = $_POST['description'];
 			if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
-				echo 'Description must be letters and spaces only.';
+				$errors['description'] = 'Description must be letters and spaces only.';
 			}
     }
 
     // Check languages
     if(empty($_POST['languages'])) {
-        echo "At least one language is required <br />";
+        $errors['languages'] = "At least one language is required <br />";
     } else {
         $ingredients = $_POST['languages'];
 		if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $languages)){
-    		echo 'Languages must be a comma separated list';
+    		$errors['languages'] = 'Languages must be a comma separated list';
 		}
     }
 } // end of POST check
@@ -66,22 +70,37 @@ if ( isset($_POST['submit']) ) {
     <section class="container grey-text">
         <h4 class="center">Add a New Project</h4>
         <form action="add.php" method="POST" class="white">
-
+            <!-- Email -->
             <label>Email: </label>
-            <input type="text" name="email">
-
+            <div class="red-text">
+                    <?php echo $errors['email']; ?>
+                </div>
+            <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+            <!-- Project Title -->  
             <label>Project title: </label>
-            <input type="text" name="title">
-
+                <div class="red-text">
+                    <?php echo $errors['title']; ?>
+                </div>
+            <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+            <!-- Image URL -->    
             <label>Image URL: </label>
-            <input type="text" name="image">
-
+                <div class="red-text">
+                    <?php echo $errors['image']; ?>
+                </div>
+            <input type="text" name="image" value="<?php echo htmlspecialchars($image) ?>">
+            <!-- Description -->
             <label>Description: </label>
-            <input type="text" name="description">
-
+                <div class="red-text">
+                    <?php echo $errors['description']; ?>
+                </div>
+            <input type="text" name="description" value="<?php echo htmlspecialchars($description) ?>">
+            <!-- Languages used -->    
             <label>Launguages used (comma separated): </label>
-            <input type="text" name="languages">
-
+                <div class="red-text">
+                    <?php echo $errors['languages']; ?>
+                </div>
+            <input type="text" name="languages" value="<?php echo htmlspecialchars($languages) ?>">
+                
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
             </div>
